b77ca172ac997f70143573e1ee39ef9ae7a0ba18
Paulius <jurgelis.paulius@gmail.com>
2024-05-06 21:38:20 +0300

    Testing and learning how interfaces work.

--- InterfaceTesting/Program.cs
+++ InterfaceTesting/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace InterfaceTesting
-{
+{ //A basic interface testing program.
     public interface IEmployeeSalary
     {
         decimal CalculateSalary();
